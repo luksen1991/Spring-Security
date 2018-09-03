aef140c86f7a0d40d6c1e4c47c970c4492964851
luksen1991 <lukasz1991code@gmail.com>
2018-09-03 12:11:49 +0200

    Override method config in SpringSecurity and set own security

--- src/main/java/pl/learningspring/carservice/WebSecurityConfig.java
+++ src/main/java/pl/learningspring/carservice/WebSecurityConfig.java
@@ -1,6 +1,7 @@
 package pl.learningspring.carservice;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -19,6 +20,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		return inMemoryUserDetailsManager;
 	}
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		http
+		.authorizeRequests()
+			.mvcMatchers("/index","index.html").permitAll()
+			.antMatchers("/css/**","/js/**","/fonts/**").permitAll()
+			.anyRequest().authenticated()
+			.and()
+		.formLogin().and()
+		.httpBasic();
+	}
+
+	
 	
 	
 }
